4d0e55b500d5ca78f358a6e866b5cc2f49970de6
Borozan-Evghenii <borozan.eugen@gmail.com>
2024-07-11 10:04:45 +0300

    feat: Create a button component

--- src/components/ui/button/Button.tsx
+++ src/components/ui/button/Button.tsx
@@ -1,48 +1,21 @@
-import React from 'react';
-import './button.css';
+import { clsx } from 'clsx';
+import React, { forwardRef } from 'react';
 
-interface ButtonProps {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  primary?: boolean;
-  /**
-   * What background color to use
-   */
-  backgroundColor?: string;
-  /**
-   * How large should the button be?
-   */
-  size?: 'small' | 'medium' | 'large';
-  /**
-   * Button contents
-   */
-  label: string;
-  /**
-   * Optional click handler
-   */
-  onClick?: () => void;
+import styles from './Button.module.css';
+
+interface ButtonProps extends React.HTMLAttributes<'button'> {
+  type?: 'primary' | 'secondary' | 'tertiary';
+  variant?: 'contained' | 'outlined' | 'text' | 'link';
+  startIcon?: React.ReactNode;
+  endIcon?: React.ReactNode;
+  children: React.ReactNode;
 }
-
-/**
- * Primary UI component for user interaction
- */
-export const Button = ({
-  primary = false,
-  size = 'medium',
-  backgroundColor,
-  label,
-  ...props
-}: ButtonProps) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
-  return (
-    <button
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-      style={{ backgroundColor }}
-      type='button'
-      {...props}
-    >
-      {label}
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ startIcon, endIcon, children, variant = 'contained', ...props }, ref) => (
+    <button className={clsx(styles.button, styles[variant])} {...props} ref={ref}>
+      {startIcon}
+      {children}
+      {endIcon}
     </button>
-  );
-};
+  )
+);
