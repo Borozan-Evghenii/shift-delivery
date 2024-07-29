107f3c4cb7e964995cf53171734b56e56479d3dd
Borozan-Evghenii <borozan.eugen@gmail.com>
2024-07-29 21:22:51 +0300

    export all delivery requests

--- src/utils/api/requests/delivery/index.ts
+++ src/utils/api/requests/delivery/index.ts
@@ -2,4 +2,6 @@ export * from './getDeliveryOrders';
 export * from './getDeliveryOrderById';
 export * from './getDeliveryPackageType';
 export * from './getDeliveryPoints';
-export * from './getDeliveryPoints';
+export * from './postDeliveryOrder';
+export * from './postDeliveryCalc';
+export * from './cancelDeliveryOrderById';
