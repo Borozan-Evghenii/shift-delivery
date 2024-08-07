5a48b235b8e98aa841f8951dcca668bf8925787b
Borozan-Evghenii <borozan.eugen@gmail.com>
2024-08-07 19:05:45 +0300

    style: styled links

--- src/components/layout/RootLayout/components/header/Header.tsx
+++ src/components/layout/RootLayout/components/header/Header.tsx
@@ -3,6 +3,7 @@ import { Link } from '@tanstack/react-router';
 import Logo from '@/assets/images/Logo.png';
 import { IconExit, IconHistory, IconUser } from '@/components/icons/svg';
 import { Button } from '@/components/ui';
+import { ROUTE } from '@/utils/constants';
 
 export const Header = () => (
   <header className='border-border-light w-full border-b-2 bg-primary py-6'>
@@ -12,22 +13,48 @@ export const Header = () => (
           <img alt='shift' src={Logo} />
         </Link>
         <nav>
-          <ul className='flex items-center gap-4'>
-            <Button asChild className='flex gap-2' colour='tertiary' variant='text'>
-              <IconUser />
-              <Link to='/profile'>History</Link>
-            </Button>
-            <Button asChild className='flex gap-2' colour='tertiary' variant='text'>
-              <IconHistory />
-              <Link to='/history'>History</Link>
-            </Button>
+          <ul className='flex items-center gap-8'>
+            <li>
+              <Button
+                asChild
+                className='font-mediu flex gap-2 p-1 text-base no-underline'
+                colour='secondary'
+                variant='link'
+              >
+                <Link className='flex gap-2' to={ROUTE.PROFILE}>
+                  <IconUser />
+                  <span>Profile</span>
+                </Link>
+              </Button>
+            </li>
+
+            <li>
+              <Button
+                asChild
+                className='font-mediu flex gap-2 p-1 text-base no-underline'
+                colour='secondary'
+                variant='link'
+              >
+                <Link className='flex gap-2' to={ROUTE.HISTORY}>
+                  <IconHistory />
+                  <span>History</span>
+                </Link>
+              </Button>
+            </li>
           </ul>
         </nav>
       </div>
 
-      <Button asChild className='flex gap-2' colour='tertiary' variant='text'>
-        <IconExit />
-        <Link to='/'>Exit</Link>
+      <Button
+        asChild
+        className='font-mediu flex gap-2 p-1 text-base no-underline'
+        colour='secondary'
+        variant='link'
+      >
+        <Link className='flex gap-2' to={ROUTE.INDEX}>
+          <IconExit />
+          <span>Exit</span>
+        </Link>
       </Button>
     </div>
   </header>
