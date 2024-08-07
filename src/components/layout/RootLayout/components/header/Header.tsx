f497a0880d44e9ad9438aec02c48e56956b6144f
Borozan-Evghenii <borozan.eugen@gmail.com>
2024-08-07 20:08:01 +0300

    header: add login and logOut button, disable navigation on auth page

--- src/components/layout/RootLayout/components/header/Header.tsx
+++ src/components/layout/RootLayout/components/header/Header.tsx
@@ -1,69 +1,91 @@
-import { Link } from '@tanstack/react-router';
+import { Link, useLocation } from '@tanstack/react-router';
 
 import Logo from '@/assets/images/Logo.png';
 import { IconExit, IconHistory, IconUser } from '@/components/icons/svg';
 import { Button } from '@/components/ui';
 import { ROUTE } from '@/utils/constants';
+import { useAuthContext } from '@/utils/contexts/session';
 
-export const Header = () => (
-  <header className='border-border-light w-full border-b-2 bg-primary py-6'>
-    <div className='container flex items-center justify-between'>
-      <div className='flex items-center gap-8'>
-        <Link to='/'>
-          <img alt='shift' src={Logo} />
-        </Link>
-        <nav>
-          <ul className='flex items-center gap-8'>
-            <li>
-              <Button
-                asChild
-                className='font-mediu flex gap-2 p-1 text-base no-underline'
-                colour='secondary'
-                variant='link'
-              >
-                <Link
-                  activeProps={{ className: 'text-primary' }}
-                  className='flex gap-2'
-                  to={ROUTE.PROFILE}
-                >
-                  <IconUser />
-                  <span>Profile</span>
-                </Link>
-              </Button>
-            </li>
+export const Header = () => {
+  const { isAuth } = useAuthContext();
+  const location = useLocation();
 
-            <li>
-              <Button
-                asChild
-                className='font-mediu flex gap-2 p-1 text-base no-underline'
-                colour='secondary'
-                variant='link'
-              >
-                <Link
-                  activeProps={{ className: 'text-primary' }}
-                  className='flex gap-2'
-                  to={ROUTE.HISTORY}
-                >
-                  <IconHistory />
-                  <span>History</span>
-                </Link>
-              </Button>
-            </li>
-          </ul>
-        </nav>
+  return (
+    <header className='border-border-light w-full border-b-2 bg-primary py-6'>
+      <div className='container flex items-center justify-between'>
+        <div className='flex items-center gap-8'>
+          <Link to='/'>
+            <img alt='shift' src={Logo} />
+          </Link>
+          {!(location.pathname === ROUTE.AUTH) && (
+            <nav>
+              <ul className='flex items-center gap-8'>
+                <li>
+                  <Button
+                    asChild
+                    className='font-mediu flex gap-2 p-1 text-base no-underline'
+                    colour='secondary'
+                    variant='link'
+                  >
+                    <Link
+                      activeProps={{ className: 'text-brand' }}
+                      className='flex gap-2'
+                      to={ROUTE.PROFILE}
+                    >
+                      <IconUser />
+                      <span>Профиль</span>
+                    </Link>
+                  </Button>
+                </li>
+
+                <li>
+                  <Button
+                    asChild
+                    className='font-mediu flex gap-2 p-1 text-base no-underline'
+                    colour='secondary'
+                    variant='link'
+                  >
+                    <Link
+                      activeProps={{ className: 'text-brand' }}
+                      className='flex gap-2'
+                      to={ROUTE.HISTORY}
+                    >
+                      <IconHistory />
+                      <span>История</span>
+                    </Link>
+                  </Button>
+                </li>
+              </ul>
+            </nav>
+          )}
+        </div>
+        {!isAuth && (
+          <Button
+            asChild
+            className='font-mediu flex gap-2 p-1 text-base no-underline'
+            colour='secondary'
+            variant='link'
+          >
+            <Link activeProps={{ className: 'text-brand' }} className='flex gap-2' to={ROUTE.AUTH}>
+              <IconExit />
+              <span>Войти</span>
+            </Link>
+          </Button>
+        )}
+        {isAuth && (
+          <Button
+            asChild
+            className='font-mediu flex gap-2 p-1 text-base no-underline'
+            colour='secondary'
+            variant='link'
+          >
+            <Link activeProps={{ className: 'text-brand' }} className='flex gap-2' to={ROUTE.INDEX}>
+              <IconExit />
+              <span>Выйти</span>
+            </Link>
+          </Button>
+        )}
       </div>
-
-      <Button
-        asChild
-        className='font-mediu flex gap-2 p-1 text-base no-underline'
-        colour='secondary'
-        variant='link'
-      >
-        <Link activeProps={{ className: 'text-primary' }} className='flex gap-2' to={ROUTE.INDEX}>
-          <IconExit />
-          <span>Exit</span>
-        </Link>
-      </Button>
-    </div>
-  </header>
-);
+    </header>
+  );
+};
