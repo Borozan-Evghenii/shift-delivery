7f90a81810c4b4c1e6bb5c55bd4e68940b358a72
Borozan-Evghenii <borozan.eugen@gmail.com>
2024-08-29 20:39:03 +0300

    feat: i have defined list of state variable needed for visualise info about created order

--- src/pages/order/components/stages/checkIn/CheckIn.tsx
+++ src/pages/order/components/stages/checkIn/CheckIn.tsx
@@ -1,1 +1,28 @@
-export const CheckIn = () => <>CheckIn</>;
+import { PageLayout } from '@/components/layout';
+import { Button, Typography } from '@/components/ui';
+
+import { useCheckInPage } from './hooks/useCheckInPage';
+
+export const CheckIn = () => {
+  const checkIn = useCheckInPage();
+
+  return (
+    <PageLayout>
+      <Typography> Проверка данных заказа</Typography>
+      <div className='flex w-[500px] flex-col gap-6'>d</div>
+      <div className='flex w-[500px] gap-6 p-4'>
+        <Button
+          className='w-full'
+          colour='secondary'
+          variant='outline'
+          onClick={() => checkIn.functions.goBack()}
+        >
+          Назад
+        </Button>
+        <Button className='w-full' colour='primary' variant='contained'>
+          Отправить
+        </Button>
+      </div>
+    </PageLayout>
+  );
+};
