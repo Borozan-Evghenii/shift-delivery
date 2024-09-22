45a5801ab8abc246bf0c87bbb277aeabb8d7043f
Borozan-Evghenii <borozan.eugen@gmail.com>
2024-09-22 16:28:14 +0300

    fix : delet dev tools

--- src/components/layout/RootLayout/RootLayout.tsx
+++ src/components/layout/RootLayout/RootLayout.tsx
@@ -1,6 +1,4 @@
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Outlet } from '@tanstack/react-router';
-import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 
 import { Header } from './components/header/Header';
 
@@ -8,7 +6,5 @@ export const RootLayout = () => (
   <>
     <Header />
     <Outlet />
-    <TanStackRouterDevtools initialIsOpen={false} />
-    <ReactQueryDevtools initialIsOpen={false} />
   </>
 );
